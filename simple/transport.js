429fa3abc15c3e718c9e16fc6dcc2ceb4e5e371b
Ladd Hoffman <ladd.hoffman@alula.net>
2021-06-14 13:51:39 -0500

    further refinement of egalitarian model

--- simple/transport.js
+++ simple/transport.js
@@ -7,21 +7,40 @@ class TransportLayer {
   constructor() {
     this.listeners = new Map();
   }
-  listen(address, callback) {
+  bind(entity, address, callback) {
+    // Return a handle we can use to send
     if (this.listeners.has(address)) {
       throw new Error('max 1 listener per address');
     }
-    this.listeners.set(address, callback);
-  }
-  // callback return value will be used as reply
-  send(address, msg, callback) {
-    const listener = this.listeners.get(address);
-    if (!listener) {
-      throw new Error(`destination address ${address} not found`);
-    }
-    const response = listener(msg);
-    if (response !== undefined) {
-      callback(response);
+    this.listeners.set(address, callback.bind(entity));
+    console.log(`${entity.getAddress()}, TransportLayer bind to address ${address}`);
+    return {
+      send: (dstAddress, msg, callback) => {
+        // callback return value will be used as reply
+        const listener = this.listeners.get(dstAddress);
+        if (!listener) {
+          throw new Error(`destination address ${dstAddress} not found`);
+        }
+        // Before sending, set the header on the outbound message.
+        msg.header = {
+          src: address,
+          dst: dstAddress,
+        };
+        // We simulate sending a message by just performing a direct call.
+        // `this` binds to the caller by default.
+        // To fix this we can explicitly bind it to the listening entity.
+        const data = listener(msg);
+        if (data !== undefined) {
+          const reply = {
+            header: {
+              src: msg.header.dst,
+              dst: msg.header.src,
+            },
+            data
+          };
+          callback(reply);
+        }
+      }
     }
   }
   list() {
